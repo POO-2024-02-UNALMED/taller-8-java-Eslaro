7d7d13848d1ab16b9b540bb0071cb0df461a3427
Eslaro <semoralesm@unal.edu.co>
2025-01-16 22:57:01 -0500

    Asi?

--- src/test/java/futbol/Jugador.java
+++ src/test/java/futbol/Jugador.java
@@ -13,9 +13,12 @@ public class Jugador extends Futbolista  {
 		this.golesMarcados = 289;
 		this.dorsal = 7;
 	}
+	public byte getDorsal() {
+		return this.dorsal;
+	}
 	@Override
 	public String toString() {
-		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de "+ this.getPosicion() + ". Ha marcado " + this.golesMarcados;
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de "+ this.getPosicion() + " con el dorsal "+ this.getDorsal() +  ". Ha marcado " + this.golesMarcados;
 	}
 
 	@Override
