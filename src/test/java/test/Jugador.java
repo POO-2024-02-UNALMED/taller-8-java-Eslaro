0cc1ae411d3abd90eeca881ea2948cfac8385a47
Eslaro <semoralesm@unal.edu.co>
2025-01-16 22:31:43 -0500

    Aqui van 3 horas y media :p

---
+++ src/test/java/test/Jugador.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Jugador extends Futbolista  {
+	private short golesMarcados;
+	private byte dorsal;
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	public Jugador() {
+		super();
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+	@Override
+	public String toString() {
+		return ((Futbolista) this).toString() + ". Ha marcado " + this.golesMarcados;
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	 public int compareTo(Futbolista otro) {
+        return Integer.compare(this.getEdad(), otro.getEdad());
+    }
+	
+}
